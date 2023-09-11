36ddc75686d8b881e94953b359337a9cc04f9670
StasGushchin <stas031094@gmail.com>
2023-09-11 23:13:12 +0300

    1 error

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
 <html lang="ru">
   <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charSet="UTF-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
     <link rel="stylesheet" href="css/style.css" />
@@ -25,7 +25,7 @@ function App() {
           <Sidebar/>
         </main>
           <AudioPlayer/>
-        <footer className="footer"></footer>
+        <footer className="footer"/>
       </div>
     </div>
   </body>
