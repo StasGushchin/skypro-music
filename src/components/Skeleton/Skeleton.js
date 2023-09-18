986fdbeac2ee11ff9ea67d23eef3795e798a2c7f
StasGushchin <stas031094@gmail.com>
2023-09-18 23:56:56 +0300

    Skeleton isnt done

--- src/components/Skeleton/Skeleton.js
+++ src/components/Skeleton/Skeleton.js
@@ -1,13 +1,19 @@
-import Skeleton from 'react-loading-skeleton'
+import React from "react"
+import ContentLoader from "react-content-loader"
 
-function BlogPost(props) {
-  return (
-    <div>
-      <h1>{props.title || <Skeleton />}</h1>
-      {props.body || <Skeleton count={10} />}
-    </div>
-  );
-}
+const Skeleton = (props) => (
+  <ContentLoader 
+    className="sidebar__item"
+    speed={2}
+    width={250}
+    height={150}
+    viewBox="0 0 250 150"
+    backgroundColor="#1f1f1f"
+    foregroundColor="#313131"
+    {...props}
+  >
+    <rect x="5" y="-12" rx="0" ry="0" width="250" height="150" />
+  </ContentLoader>
+)
 
 export default Skeleton
-
