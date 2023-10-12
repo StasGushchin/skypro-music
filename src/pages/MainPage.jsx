7e49217d8fda01fa09f0fa21aed272c076d0e39f
StasGushchin <stas031094@gmail.com>
2023-10-12 23:17:57 +0300

    Get api

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NavMenu from "../components/NavMenu/NavMenu";
 import AudioPlayer from "../components/AudioPlayer/AudioPlayer";
 import Tracklist from "../components/Tracklist/Tracklist";
 import Sidebar from "../components/Sidebar/Sidebar";
 import * as S from '../App.styles'
+import { getAllTracks } from "../api";
 
 export const MainPage = () => {
+
     return (
     <S.Wrapper>
       <S.Container>
