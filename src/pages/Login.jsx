4b9b347babefc63875e3f6ab26557eac93d83988
StasGushchin <stas031094@gmail.com>
2023-10-16 19:07:20 +0300

    Login works

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,13 +1,16 @@
-import { Navigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import * as S from './Login.styles'
 
 export const Login = ({ setAuth }) => {
+const navigate = useNavigate()
   return (
     <div>
       <S.Title>Login</S.Title>
-        <button onClick={
-        setAuth
-        }>Войти</button>
+      <button onClick={() => {
+        setAuth();
+        navigate('/', { replace: false })
+      }
+      }>Войти</button>
     </div>
   )
 }
